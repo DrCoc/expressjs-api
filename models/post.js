bbbe63ee0630db4657e27494e7851a4cb742ff09
ALLEMAND Corentin <corentin.allemand@trsb.net>
2018-05-28 23:38:42 +0200

    Database configuration

---
+++ models/post.js
@@ -0,0 +1,18 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var Post = sequelize.define('Post', {
+    idUSERS: DataTypes.INTEGER,
+    message: DataTypes.STRING,
+    attachment: DataTypes.STRING,
+    like: DataTypes.INTEGER
+  }, {});
+  Post.associate = function(models) {
+    // associations can be defined here
+      models.Message.belongsTo(models.User, {
+        foreignKey : {
+          allowNull: false
+        }
+      })
+  };
+  return Post;
+};
